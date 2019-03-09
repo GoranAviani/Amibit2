3f1510ea4dae33646761ef2e736653541e00917f
goranaviani <goranaviani@gmail.com>
2019-03-09 17:47:03 +0100

    expandeduser model

--- expandeduser/models.py
+++ expandeduser/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+
+class custom_user(AbstractUser):
+    userMobilenumber = models.CharField(max_length=50,null=True, blank=True)
+    # userAddress = models.CharField(max_length=30,null=True, blank=True)
+    userCity = models.CharField(max_length=30,null=True, blank=True)
+    userCountry = models.CharField(max_length=30,null=True, blank=True)
+    userDoB = models.DateField(null=True, blank=True)
+    userSecondEmail = models.email(max_length=50,null=True, blank=True)
+
+
+    def __str__(self):
+return self.email
