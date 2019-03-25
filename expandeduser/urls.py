a86d36dd9119773b73ff0c4b065adffa3f61abf5
goranaviani <goranaviani@gmail.com>
2019-03-25 16:35:45 +0100

    user settings menu

--- expandeduser/urls.py
+++ expandeduser/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('signup/', views.sign_up.as_view(), name='signup'),
     path('', include('social_django.urls', namespace='social')),
+    path('setttings/', views.user_settings_menu, name='user_settings_menu'),
 ]
--- expandeduser/views.py
+++ expandeduser/views.py
@@ -10,3 +10,26 @@ class sign_up(generic.CreateView):
     form_class = custom_user_creation_form
     success_url = reverse_lazy('dashboard') #TODO 
     template_name = 'signup.html'
+
+def user_settings_menu(request):
+     return render(
+         request,
+        'expanded_user/user_settings_menu.html',
+)
+
+'''
+def user_info_edit_profile(request):
+    if request.method == 'POST':
+        user_info_form = UserInfo(request.POST, instance = request.user)
+        if user_info_form.is_valid():
+            user_info_form.save()
+            return redirect('user_info')
+        else:
+            user_info_form = UserInfo(instance=request.user)
+            return render (request, 'user/user_info_edit_profile.html', {'user_info_form' : user_info_form})
+
+    else:
+        user_info_form = UserInfo(instance=request.user)
+    return render (request, 'user/user_info_edit_profile.html', {'user_info_form' : user_info_form})
+
+'''
