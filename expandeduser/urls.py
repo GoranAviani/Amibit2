934f1986ab5bab675faad9b88bdfc9795aee5a18
goranaviani <goranaviani@gmail.com>
2019-04-06 18:39:50 +0200

    pasword reset view

--- expandeduser/urls.py
+++ expandeduser/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('signup/', views.sign_up.as_view(), name='signup'),
@@ -10,4 +11,5 @@ urlpatterns = [
     path('profile/edit-info/', views.edit_user_profile, name='edit_user_profile'),
     path('profile/edit-password/', views.edit_user_password, name='edit_user_password'),
 
+    path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
 ]
