a4b902e8c9c7e98a9a1d469d70d9e9c592dc3b53
goranaviani <goranaviani@gmail.com>
2019-04-12 20:42:13 +0200

    link model

--- notelink/models.py
+++ notelink/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+class Link(models.Model):
+    link_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    link_name = models.CharField(max_length=200)
+    link_url = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.link_name
