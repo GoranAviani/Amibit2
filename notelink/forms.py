253587b4a62d9dddcddbf99b39ffd2c2f104ffb4
goranaviani <goranaviani@gmail.com>
2019-04-12 21:48:54 +0200

    renaming of link model to lower case

--- notelink/forms.py
+++ notelink/forms.py
@@ -1,12 +1,12 @@
 from django import forms
 #from .models import custom_user
-from .models import Link
+from .models import link
 
 class create_link_form(forms.ModelForm):
     linkName = forms.CharField(label='', widget=forms.TextInput(attrs={'class':'note-title-input'}))
     linkUrl = forms.CharField(label='', widget=forms.TextInput(attrs={'class':'note-title-input'}))
     class Meta:
-        model = Link
+        model = link
         fields = (
             'linkName',
             'linkUrl',
--- notelink/models.py
+++ notelink/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.conf import settings
 # Create your models here.
 
-class Link(models.Model):
+class link(models.Model):
     link_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     link_name = models.CharField(max_length=200)
     link_url = models.CharField(max_length=1000)
