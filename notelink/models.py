b18756d2e452afcc73a79510005f03a1f5b41f24
goranaviani <goranaviani@gmail.com>
2019-04-13 21:04:45 +0200

    note model

--- notelink/models.py
+++ notelink/models.py
@@ -9,3 +9,14 @@ class link(models.Model):
 
     def __str__(self):
         return self.linkName
+
+
+class Note(models.Model):
+    noteUser = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    noteTitle = models.CharField(max_length=200)
+    noteText = models.TextField()
+    noteTimestamp = models.DateTimeField(auto_now_add=True) #auto_now_add time the instance was created
+    noteSlug = models.SlugField(max_length=250)
+    
+    def __str__(self):
+        return self.noteTitle
