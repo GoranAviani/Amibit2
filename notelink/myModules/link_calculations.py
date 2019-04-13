45628602ed64f119a85a36852438ad3772b910f9
goranaviani <goranaviani@gmail.com>
2019-04-13 15:09:02 +0200

    checking and adding http:// on links if it is missins

--- notelink/myModules/link_calculations.py
+++ notelink/myModules/link_calculations.py
@@ -1,2 +1,23 @@
 def check_url_link(urlLink):
+    urlLink = check_https_on_link(urlLink)
     return urlLink
+
+
+
+
+#Check does the link have http or https on the beginning, if it does not add it
+def check_https_on_link(urlLink):
+    HTTP_URL = "http://"
+    HTTPS_URL = "https://"
+    if urlLink[0:7] != HTTP_URL:
+        if urlLink[0:8] != HTTPS_URL:
+            urlLink = HTTP_URL + urlLink
+    return urlLink
+
+
+
+def main():
+    pass
+
+if __name__=="__main__":
+    main()
