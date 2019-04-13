904652ea1b277e84e4d4b437531ec615a9987c47
goranaviani <goranaviani@gmail.com>
2019-04-13 17:12:53 +0200

    set google as default search in search bar

--- amibit2/myModules/search_bar.py
+++ amibit2/myModules/search_bar.py
@@ -4,12 +4,12 @@ def get_web_url(text):
     HTTP_URL = "http://"
     HTTPS_URL = "https://"
 
-    if text[0:2+1] == "go ":
+    if text[0:2+1] == "go ": #open direct page
         return (HTTP_URL+text[3:])
     elif text[0:3+1] == "goo ":
-        return (HTTP_URL+"www.google.com/?#q="+(text[4:]))
+        return (HTTP_URL+"www.google.com/?#q="+(text[4:])) #search with..
     elif text[0:3+1] == "ddg ":
-        return (HTTP_URL+"www.duckduckgo.com/?q="+(text[4:]))
+        return (HTTP_URL+"www.duckduckgo.com/?q="+(text[4:])) #search with..
     else:
-        return (HTTP_URL+"www.duckduckgo.com/?q="+(text))
+        return (HTTP_URL+"www.google.com/?q="+(text)) #search with...
 
