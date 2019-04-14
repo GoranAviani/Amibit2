c874a08f4ec3399dd3e4f3a05f1356c627662b25
goranaviani <goranaviani@gmail.com>
2019-04-14 18:32:41 +0200

    create_note_form

--- notelink/forms.py
+++ notelink/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 #from .models import custom_user
-from .models import link
+from .models import link, note
 
 class create_link_form(forms.ModelForm):
     linkName = forms.CharField(label='', widget=forms.TextInput(attrs={'class':'note-title-input'}))
@@ -10,4 +10,16 @@ class create_link_form(forms.ModelForm):
         fields = (
             'linkName',
             'linkUrl',
+)
+
+
+
+class create_note_form(forms.ModelForm):
+    note_title = forms.CharField(label='', widget=forms.TextInput(attrs={'class':'note-title-input'}))
+    note_text = forms.CharField(label='', widget=forms.Textarea(attrs={'class':'note-text-input'}))
+    class Meta:
+        model = note
+        fields = (
+            'note_title',
+            'note_text',
 )
