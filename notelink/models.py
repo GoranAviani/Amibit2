bacd9d102ef2307a06a0dd9056af68aa294ef033
goranaviani <goranaviani@gmail.com>
2019-04-15 17:37:27 +0200

    changed noteTimestamp to editable field

--- notelink/models.py
+++ notelink/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 # Create your models here.
+import datetime
 
 class link(models.Model):
     linkUser = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -15,7 +16,7 @@ class note(models.Model):
     noteUser = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     noteTitle = models.CharField(max_length=200)
     noteText = models.TextField()
-    noteTimestamp = models.DateTimeField(auto_now_add=True) #auto_now_add time the instance was created
+    noteTimestamp = models.DateTimeField(default=datetime.datetime.now) #auto_now_add time the instance was created
     noteSlug = models.SlugField(max_length=250)
     
     def __str__(self):
