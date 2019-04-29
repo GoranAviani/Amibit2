d863044cd3b44fc9f9c59e7d075028b5edd7cc18
goranaviani <goranaviani@gmail.com>
2019-04-29 16:23:37 +0200

    On screens smaller than 768 all dashboard notes and links are going to be hiden and NOTES , LINKS buttons will be visible. Pressing those links will show the notes and/or links for a current user

--- static/js/notelink.js
+++ static/js/notelink.js
@@ -1,3 +1,36 @@
+var screen_width = window.innerWidth;
+var screen_height = window.innerHeight;
+
+window.onload = function() {
+    if(screen_width <= 768) {
+      document.getElementById("DashboardNotes").style.display = "none";
+      document.getElementById("DashboardLinks").style.display = "none";
+    }
+  };
+  
+
+
+  function toggle_notes() {
+    var x = document.getElementById("DashboardNotes");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function toggle_links() {
+    var x = document.getElementById("DashboardLinks");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+
+
+
 function confirmLinkDeletion() {
     var res = confirm("You are about to delete a link. Are you sure?");
       if (res) {
@@ -16,3 +49,4 @@ var res = confirm("You are about to delete a note. Are you sure?");
         return false;
     }
 }
+
