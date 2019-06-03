59c6569e76f58f7f580db14aec43fe4cf2318150
goranaviani <goranaviani@gmail.com>
2019-06-03 22:28:33 +0200

    debug true

--- amibit2/settings.py
+++ amibit2/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['104.248.28.51','www.amibit.org','amibit.org','localhost']
 
