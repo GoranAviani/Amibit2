16f3c7e65a32ea1efe77b01e055c993e0abc8b54
goranaviani <goranaviani@gmail.com>
2019-06-03 20:43:50 +0200

    added local host to allowed hosts

--- amibit2/settings.py
+++ amibit2/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['104.248.28.51','www.amibit.org','amibit.org']
+ALLOWED_HOSTS = ['104.248.28.51','www.amibit.org','amibit.org','localhost']
 
 
 # Application definition
