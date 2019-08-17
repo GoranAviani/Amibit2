e3db65b687d7c6a6a7f8766d9293d1ae12e1050f
goranaviani <goranaviani@gmail.com>
2019-08-17 14:59:39 +0200

    Added edit user phone template and button on the user profile template

--- mobile_phone/views.py
+++ mobile_phone/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .forms import (
+    user_mobile_phone_form
+)
+
+
+def edit_user_phone(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            user_profile_form_data = user_profile_form(request.POST, instance = request.user)
+            if user_profile_form_data.is_valid():
+                user_profile_form_data.save()
+                return redirect('user_profile')
+        else:
+            user_phone_form_data = user_mobile_phone_form(instance=request.user)
+            return render (request, 'mobile_phone/edit_user_phone.html', {'user_phone_form_data' : user_phone_form_data})
+    else:
+        return render(request,'index.html')
