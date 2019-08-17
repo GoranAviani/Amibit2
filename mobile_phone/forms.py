933e8448302f8cc08ebaa1062879c8af29a8a92b
goranaviani <goranaviani@gmail.com>
2019-08-17 19:19:29 +0200

    Fixed - imputting user phone no - if this is the first time the user is inputting his phone number

--- mobile_phone/forms.py
+++ mobile_phone/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import user_phone
 
 class user_mobile_phone_form(forms.ModelForm):
-    userMobilePhone = forms.CharField(label='', widget=forms.TextInput(attrs={'readonly':'readonly','class':'form-control'}))
+    #userMobilePhone = forms.CharField(label='', widget=forms.TextInput(attrs={'readonly':'readonly','class':'form-control'}))
     phoneCountryCode = forms.CharField(label='', widget=forms.TextInput(attrs={'class':'form-control'}))
     phoneNumber = forms.CharField(label='', widget=forms.TextInput(attrs={'class':'form-control'}))
     class Meta:
--- mobile_phone/views.py
+++ mobile_phone/views.py
@@ -8,20 +8,41 @@ from .models import (
     user_phone
 )
 
+from expandeduser.models import custom_user
+
 def edit_user_phone(request):
     if request.user.is_authenticated:
         if request.method == 'POST':
-            user_phone_form_data = user_mobile_phone_form(request.POST, )
-            if user_phone_form_data.is_valid():
-                form = user_phone_form_data.save(commit=False)
-                form.userMobilePhone = request.user
-                form.save()
-               # messages.success(request, 'Note saved!',extra_tags='create_note')
+            user_phone_form_data = user_mobile_phone_form(request.POST.copy()) #.copy() because query dict would be immutable
+          
+            
+            
+          
+            try:
+                #if this user is found in user phone model ergo already has his phone number here
+                found_u_p_data = user_phone.objects.get(userMobilePhone=request.user)
+                data = {'userMobilePhone':found_u_p_data.userMobilePhone, 'phoneCountryCode': found_u_p_data.phoneCountryCode, "phoneNumber" : found_u_p_data.phoneNumber}
+                user_phone_form_data = user_mobile_phone_form(initial=data)
+
+            except:
+                #if this is the first time the user is inputting his phone number
+                
+                aa = custom_user.objects.get(pk=request.user.pk)
+                user_phone_form_data.data["userMobilePhone"] = aa
+                # user_phone_form_data.data["userMobilePhone"] =  request.user
+                #print(user_phone_form_data["userMobilePhone"].data())
+                if user_phone_form_data.is_valid():
+                    form = user_phone_form_data.save(commit=False)
+                    form.userMobilePhone = request.user
+                    form.phoneCountryCode =  user_phone_form_data["phoneCountryCode"].data
+                    form.phoneNumber = user_phone_form_data["phoneNumber"].data
+                    form.save()
+                # messages.success(request, 'Note saved!',extra_tags='create_note')
             return redirect('edit_mobile_phone')
         else:
             try:
-                found_u_p_data = user_phone.objects.get(userMobilePhone=request.user.pk)
-                data = {'phoneCountryCode': found_u_p_data.phoneCountryCode, "phoneNumber" : found_u_p_data.phoneNumber}
+                found_u_p_data = user_phone.objects.get(userMobilePhone=request.user)
+                data = {'userMobilePhone':found_u_p_data.userMobilePhone, 'phoneCountryCode': found_u_p_data.phoneCountryCode, "phoneNumber" : found_u_p_data.phoneNumber}
                 user_phone_form_data = user_mobile_phone_form(initial=data)
             except:
                 user_phone_form_data = user_mobile_phone_form()
