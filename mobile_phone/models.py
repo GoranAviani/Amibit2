de33a0934116249a22244e02aff7942209871f48
goranaviani <goranaviani@gmail.com>
2019-08-17 13:50:57 +0200

    Added user phone model

--- mobile_phone/models.py
+++ mobile_phone/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from django.conf import settings
+
+
+class user_phone(models.Model):
+    userMobilePhone = models.ForeignKey(settings.AUTH_USER_MODEL, unique= True, on_delete=models.CASCADE)
+    phoneCountryCode = models.CharField(max_length=6)
+    phoneNumber = models.CharField(max_length=12)
+    isMobileValidated = models.BooleanField(default = False)
+    
+    def __str__(self):
+        return self.phoneNumber
