0aca34532e02ea7c5f040b00f8c5ec5dd93b553f
goranaviani <goranaviani@gmail.com>
2019-08-19 21:12:36 +0200

    set for fixing 500

--- amibit2/settings.py
+++ amibit2/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['104.248.28.51','www.amibit.org','amibit.org','localhost','127.0.0.1']
 
